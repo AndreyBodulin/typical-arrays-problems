e01ccf837fd24156ac233a7d9dd10954afa04981
AndreyBodulin <andrey_bodulin@mail.ru>
2021-03-14 13:50:19 +0400

    task: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 10000;
+  console.log(array);
+  if (typeof array != 'undefined'){
+    for (let i = 0; i < array.length; i++){
+      if (array[i] < min) min = array[i];
+    }
+  }
+  if (min == 10000) min = 0;
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = -10000;
+  if (typeof array != 'undefined'){
+    for (let i = 0; i < array.length; i++){
+      if (array[i] > max) max = array[i];
+    }
+  }
+  if (max == -10000) max = 0;
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  let sum = 0;
+  if (typeof array != 'undefined'){
+    for (let i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    if (array.length != 0) avg = sum / array.length;
+  }
+  return avg;
 }
